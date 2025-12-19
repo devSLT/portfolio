6580be2e1f502b0c5a4436acb6f5cf66cfa594d2
devSLT <thiago368orjorge@gmail.com>
2025-12-18 23:55:03 -0300

    first page responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import "../styles/Header.css"
 
 function Header() {
     return (
-        <header className="w-full h-[100px] flex flex-col justify-center items-center font-[var(--robotoflex)] font-bold">
+        <header className="w-full max-w-full overflow-hidden h-[100px] flex flex-col justify-center items-center font-[var(--robotoflex)] font-bold ">
             <div className="topHeader w-full h-3/5 flex justify-center items-center">
-                <h2 className="text-2xl text-[var(--whitePearl)]">thiago<span className="markRed">.dev</span></h2>
+                <h2 className="text-2xl md:text-3xl text-[var(--whitePearl)]">
+                    thiago<span className="markRed">.dev</span>
+                </h2>
             </div>
             <nav className="w-4/5 h-2/5 flex justify-between items-center">
-                <ul className="w-full h-full flex justify-between items-center list-none">
-                    <li><a href="#About">About</a></li>
-                    <li><a href="#Project">Project</a></li>
-                    <li><a href="#Contact">Contact</a></li>
+                <ul className="w-full h-full flex justify-between items-center list-none font-semibold md:font-bold text-base md:text-lg text-[var(--whitePearl)]">
+                    <li><a href="#About" className="decoration none">About</a></li>
+                    <li><a href="#Projects" className="decoration none">Projects</a></li>
+                    <li><a href="#Contact" className="decoration none">Conta</a></li>
                 </ul>
             </nav>
         </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,16 @@ import "../styles/Hero.css"
 
 function Hero() {
     return (
-        <div className="heroPage w-full h-[calc(100dvh-100px)] flex flex-col items-center justify-center">
-            <div className="heroContent w-4/5 font-bold ">
-                <h1 className="text-8xl text-[var(--whitePearl)]">
+        <div className="heroPage w-full w-max-full overflow-hidden h-[calc(100dvh-100px)] flex flex-col items-center justify-center">
+            <div className="heroContent w-4/5 font-bold">
+                <h1 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-[var(--whitePearl)]">
                     Olá,<br />
                     eu sou <span className="markRed">Thiago</span>,<br />
                     desenvolvedor full stack
                 </h1>
-                <h2 className="block text-lg text-[var(--whitePearl)]">
-                    Possuo experiência em HTML, CSS, JavaScript, React, React Native e bancos de dados SQL e NoSQL. Formado em Técnico em Informática para Internet pelo SESI/SENAI (2024), atualmente curso Ciências da Computação na Universidade Católica de Brasília. Gosto de transformar ideias em soluções digitais eficientes e bem estruturadas.
-                </h2>
-                <button className="relative w-xs h-[60px] text-2xl cursor-pointer bg-transparent font-bold text-[var(--red)] rounded-xl">
+                <h2 className="block text-base md:text-xl font-normal text-[var(--whitePearl)]">
+                    Desenvolvedor focado em criar soluções digitais eficientes, bem estruturadas e orientadas à experiência do usuário.                </h2>
+                <button className="relative w-48 md:w-xs h-[60px] text-xl lg:text-2xl cursor-pointer bg-transparent font-bold text-[var(--red)] rounded-xl">
                     Ver Projetos
                 </button>
             </div>
